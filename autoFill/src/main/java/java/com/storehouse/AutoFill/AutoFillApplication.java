f0e301a90ee5ff56e583568f5eab040a4a70f73c
DimaOstanin <xnadab@gmail.com>
2024-04-21 23:00:44 +0300

    autofill init

---
+++ autoFill/src/main/java/java/com/storehouse/AutoFill/AutoFillApplication.java
@@ -0,0 +1,13 @@
+package java.com.storehouse.AutoFill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFillApplication.class, args);
+	}
+
+}
